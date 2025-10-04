5acbc2aa390df921b92d9fb543c3467dada6105c
Soumen Goswami <72848981+Soumen392@users.noreply.github.com>
2025-10-04 10:01:42 +0530

    Update App.jsx

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -11,6 +11,9 @@ import UrlShortener from "./pages/UrlShortener";
 import UuidGenerator from "./pages/UuidGenerator";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
+import PdfCompressor from "./pages/PdfCompressor";
+import ImageToPdf from "./pages/ImageToPdf";
+
 
 // Public layout (without Navbar & Footer)
 const PublicLayout = ({ children }) => <>{children}</>;
@@ -28,94 +31,112 @@ const PrivateLayout = ({ children }) => {
 
 const App = () => {
   return (
-    <Routes>
-      {/* Public pages */}
-      <Route
-        path="/"
-        element={
-          <PublicLayout>
-            <Intro />
-          </PublicLayout>
-        }
-      />
-      <Route
-        path="/login"
-        element={
-          <PublicLayout>
-            <Login />
-          </PublicLayout>
-        }
-      />
-      <Route
-        path="/signup"
-        element={
-          <PublicLayout>
-            <SignUp />
-          </PublicLayout>
-        }
-      />
+    <>
+      <Routes>
+        {/* Public pages */}
+        <Route
+          path="/"
+          element={
+            <PublicLayout>
+              <Intro />
+            </PublicLayout>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <PublicLayout>
+              <Login />
+            </PublicLayout>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <PublicLayout>
+              <SignUp />
+            </PublicLayout>
+          }
+        />
 
-      {/* Private pages (after login) */}
-      <Route
-        path="/home"
-        element={
-          <PrivateLayout>
-            <Home />
-          </PrivateLayout>
-        }
-      />
-      <Route
-        path="/about"
-        element={
-          <PrivateLayout>
-            <About />
-          </PrivateLayout>
-        }
-      />
-      <Route
-        path="/contact"
-        element={
-          <PrivateLayout>
-            <Contact />
-          </PrivateLayout>
-        }
-      />
+        {/* Private pages (after login) */}
+        <Route
+          path="/home"
+          element={
+            <PrivateLayout>
+              <Home />
+            </PrivateLayout>
+          }
+        />
+        <Route
+          path="/tools/img-to-pdf"
+          element={
+            <PrivateLayout>
+              <ImageToPdf />
+            </PrivateLayout>
+          }
+        />
+        <Route
+          path="/tools/pdf-compressor"
+          element={
+            <PrivateLayout>
+              <PdfCompressor />
+            </PrivateLayout>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <PrivateLayout>
+              <About />
+            </PrivateLayout>
+          }
+        />
+        <Route
+          path="/contact"
+          element={
+            <PrivateLayout>
+              <Contact />
+            </PrivateLayout>
+          }
+        />
 
-      {/* Future Tools  */}
+        {/* Future Tools  */}
 
-      <Route
-        path="/tools/qr"
-        element={
-          <PrivateLayout>
-            <QrTool />
-          </PrivateLayout>
-        }
-      />
-      <Route
-        path="/tools/url"
-        element={
-          <PrivateLayout>
-            <UrlShortener />
-          </PrivateLayout>
-        }
-      />
-      <Route
-        path="/tools/password"
-        element={
-          <PrivateLayout>
-            <PasswordGen />
-          </PrivateLayout>
-        }
-      />
-      <Route
-        path="/tools/uuid"
-        element={
-          <PrivateLayout>
-            <UuidGenerator />
-          </PrivateLayout>
-        }
-      />
-    </Routes>
+        <Route
+          path="/tools/qr"
+          element={
+            <PrivateLayout>
+              <QrTool />
+            </PrivateLayout>
+          }
+        />
+        <Route
+          path="/tools/url"
+          element={
+            <PrivateLayout>
+              <UrlShortener />
+            </PrivateLayout>
+          }
+        />
+        <Route
+          path="/tools/password"
+          element={
+            <PrivateLayout>
+              <PasswordGen />
+            </PrivateLayout>
+          }
+        />
+        <Route
+          path="/tools/uuid"
+          element={
+            <PrivateLayout>
+              <UuidGenerator />
+            </PrivateLayout>
+          }
+        />
+      </Routes>
+    </>
   );
 };
 
