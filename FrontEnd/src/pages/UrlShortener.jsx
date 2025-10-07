a86e3af7c768da662d51b2436f204182d34305ee
coAyush <chowdhuryayush7@gmail.com>
2025-10-07 18:16:55 +0530

    Initial commit

--- FrontEnd/src/pages/UrlShortener.jsx
+++ FrontEnd/src/pages/UrlShortener.jsx
@@ -58,7 +58,7 @@ const UrlShortener = () => {
         headers: { "Content-Type": "application/json" },
       });
 
-      const shortUrl = `${window.location.origin}/r/${code}`;
+      const shortUrl = `${window.location.origin}/r/${data}`;
 
       // Check if we received a valid-looking string
       if (shortUrl && typeof shortUrl === "string") {
